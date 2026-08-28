9ff865521fe611f336887ada199f741833a1be89
KelvinLeandroMR <kelvinleaorodrigues011@gmail.com>
2026-08-27 21:40:56 -0300

    Delete CRUD_Mundo_Seguro directory

--- CRUD_Mundo_Seguro/app/views/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer class="footer">
-    <div class="container">CRUD Mundo · PHP + MySQL · Aplicação acadêmica</div>
-</footer>
-<script src="assets/js/app.js"></script>
-</body>
-</html>
--- CRUD_Mundo_Seguro/public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../app/lib/security.php';
-
-if (is_logged_in()) {
-    audit('Logout realizado.');
-}
-logout_user();
-redirect('index.php');
